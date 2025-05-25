43f5bfbc5df6802dee9a2e71eb865ac13a9a9239
lelis685 <lelis685@gmail.com>
2025-05-24 22:44:27 -0300

    test micrometer statsD

--- datadog/src/main/java/com/example/datadog/config/MetricsHelper.java
+++ datadog/src/main/java/com/example/datadog/config/MetricsHelper.java
@@ -1,6 +1,8 @@
 package com.example.datadog.config;
 
 import com.timgroup.statsd.StatsDClient;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -16,9 +18,13 @@ public class MetricsHelper {
     @Autowired
     private StatsDClient statsDClient;
 
+    @Autowired
+    private MeterRegistry meterRegistry;
+
 
     public void incrCounter(String metricName, String... tags) {
         statsDClient.incrementCounter(metricName, createTag(tags));
+        meterRegistry.counter(metricName, tags).increment();
     }
 
     public void setGauge(String metricName, Integer value, String... tags) {
@@ -27,6 +33,13 @@ public class MetricsHelper {
 
     public void setDistribution(String metricName, Integer value, String... tags) {
         statsDClient.recordDistributionValue(metricName, value, createTag(tags));
+        statsDClient.recordHistogramValue(metricName + "_histogram", value, createTag(tags));
+        DistributionSummary.builder(metricName)
+                .tags(tags)
+                .publishPercentileHistogram(false)
+                .publishPercentiles(0.5, 0.95, 0.99)
+                .register(meterRegistry)
+                .record(value);
     }
 
 
